15dab7585d5bce3bd90fd910eb61842ccef7eaba
mehul kothari <mehulkothari05@gmail.com>
2024-08-16 16:18:27 +0530

    intial commit

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js', // Entry point
+  output: {
+    filename: '[name].[contenthash].js', // this will generate unique file names
+    path: path.resolve(__dirname, 'dist'), // Output directory,
+    clean: false, // Ensure clean is false so older files aren't deleted
+
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/, // Apply Babel to JS files
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  mode: 'development', // Development mode
+};
